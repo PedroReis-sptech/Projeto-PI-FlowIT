4278fe12eb3ce14a8304f2725eed637badc3ba15
Larissamplopez1 <larissa.lopez@sptech.school>
2026-05-18 15:04:31 -0300

    Alteração no Usuario Model
    Alteração no Usuario Model

--- src/models/usuarioModel.js
+++ src/models/usuarioModel.js
@@ -11,9 +11,28 @@ function autenticar(email, senha) {
 // CORRIGIDO: Adicionado o parâmetro 'codigo' que estava faltando na assinatura da função
 function cadastrar(nome, email, senha, codigo) {
     console.log("ACESSEI O USUARIO MODEL \n \n\t\t >> Se aqui der erro de 'Error: connect ECONNREFUSED',\n \t\t >> verifique suas credenciais de acesso ao banco\n \t\t >> e se o servidor de seu BD está rodando corretamente. \n\n function cadastrar():", nome, email, senha, codigo); 
-    var instrucaoSql = `
-        INSERT INTO usuario (codigoVerificacao, nomeUsuario, email, senha) VALUES ('${codigo}','${nome}', '${email}', '${senha}');
+     var codigosql = `
+        SELECT codigoVerificacao FROM loja;
     `;
+    let resultado = database.executar(codigosql);
+    console.log(resultado);
+    let codigoCerto = false;
+    
+    for(let i = 0; i < resultado.length; i++){
+        if(resultado[i] == codigo){
+            codigoCerto = true
+        }
+    }
+    if(codigoCerto){
+        console.log('oi')
+        var instrucaoSql = `
+        INSERT INTO usuario (nomeUsuario, email, senha) VALUES ('${nome}', '${email}', '${senha}');
+    `;
+    return;
+    }else {
+        console.log('Erro do código de verificação')
+    }
+    
     console.log("Executando a instrução SQL: \n" + instrucaoSql);
     return database.executar(instrucaoSql);
 }
