1dfa819827d45fc3e83086ac42614e8c95b7cb35
Larissa-Cardozo <larissa.cardozo@sptech.school>
2026-06-03 15:39:15 -0300

    Alterando controller e html da function da tela de gestão

--- src/controllers/gestaoController.js
+++ src/controllers/gestaoController.js
@@ -2,9 +2,9 @@ var gestaoModel = require("../models/gestaoModel");
 
 
 function listarSetores(req, res) {
-  var id = req.params.id;
+  var id = req.params.idLoja;
 
-  gestaoModel.listarSetores().then((resultado) => {
+  gestaoModel.listarSetores(id).then((resultado) => {
     res.status(200).json(resultado);
   });
 }
